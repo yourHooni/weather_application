a6ee75d8c3e6e5e8b36c2202b1324ebce6649970
jhoon6886@gmail.com <pwhyun6246>
2022-07-17 23:50:41 +0900

    feat: weather service
    - ADD: weather service
    - ADD: service 관련 env

---
+++ src/services/weather.ts
@@ -0,0 +1,13 @@
+const url = `${process.env.REACT_APP_WEATHER_BASE_URL}/weather`;
+
+export const getWeatherDataWithApi = async (cityID: string) => {
+  const _url = `${url}?id=${cityID}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+  
+  const response = await fetch(_url);
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  } else {
+    alert('error');
+  }
+};
