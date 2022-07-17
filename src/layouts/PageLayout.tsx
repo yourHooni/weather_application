87f16a6c451e0b908ffd7001602acf3e654a4ab1
jhoon6886@gmail.com <pwhyun6246>
2022-07-17 14:56:14 +0900

    fix: PageLayout
    UPDATE: 코드정리
    DELETE: searchParams 관련 로직 -> WeatherInfoPage에 추가

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,6 +1,3 @@
-
-import React, { useState, useEffect, useRef, PropsWithChildren, SyntheticEvent, Fragment } from 'react';
-import { Link, Route, Routes, useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 /* Pages */
@@ -11,13 +8,10 @@ import BaseContainer from 'components/BaseContainer';
 import SearchFragment from 'components/SearchFragment';
 
 const Title = styled.h1`
-  margin-bottom: 30px;
+  margin-bottom: 20px;
 `;
 
 function PageLayout() {
-  let [searchParams, setSearchParams] = useSearchParams();
-  let city = searchParams.get("city");
-
   return (
     <BaseContainer
       flex
@@ -28,11 +22,7 @@ function PageLayout() {
     >
       <Title> Search Weather Data </Title>
       <SearchFragment />
-      {
-        city && (
-          <WeatherInfoPage />
-        )
-      }
+      <WeatherInfoPage />
     </BaseContainer>
   );
 }
